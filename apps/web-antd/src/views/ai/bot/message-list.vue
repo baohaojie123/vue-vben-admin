64abdc085e46e0a572a8e79aa5fe42a3c4e65f8b
meishijian <1323101667@qq.com>
2025-05-16 11:55:57 +0800

    refactor(消息列表): 使用VxeTable重构消息列表组件
    将消息列表组件从简单的标题显示重构为使用VxeTable表格组件，以支持更复杂的数据展示和操作功能。移除了对路由参数的依赖，并定义了表格的列配置和工具栏功能。

--- apps/web-antd/src/views/ai/bot/message-list.vue
+++ apps/web-antd/src/views/ai/bot/message-list.vue
@@ -1,12 +1,54 @@
 <script lang="ts" setup>
-import { useRoute } from 'vue-router';
+import type { VxeTableGridOptions } from '#/adapter/vxe-table';
 
 import { Page } from '@vben/common-ui';
 
-const route = useRoute();
+import { useVbenVxeGrid } from '#/adapter/vxe-table';
+
+defineOptions({
+  name: 'MessageList',
+});
+
+interface RowType {
+  id: string;
+  clBotId: string;
+  cozeBotId: string;
+  createTime: string;
+  name: string;
+  userId: string;
+}
+const gridOptions: VxeTableGridOptions<RowType> = {
+  columns: [
+    { field: 'id', title: 'Coze会话ID' },
+    { field: 'clBotId', title: 'cl智能体ID' },
+    { field: 'cozeBotId', title: 'coze智能体ID' },
+    { field: 'userId', title: 'Coze会话userId' },
+    { field: 'name', title: '会话名称' },
+    { field: 'createTime', title: '创建时间' },
+  ],
+  // data: [],
+  exportConfig: {},
+  height: 'auto',
+  keepSource: true,
+  pagerConfig: {},
+  proxyConfig: {},
+  toolbarConfig: {
+    custom: true,
+    export: true,
+    refresh: true,
+    resizable: true,
+    search: true,
+    zoom: true,
+  },
+};
+const [Grid] = useVbenVxeGrid({
+  gridOptions,
+});
+
+// const route = useRoute();
 </script>
 <template>
   <Page>
-    <h3>消息列表{{ route.query.id }}</h3>
+    <Grid />
   </Page>
 </template>
